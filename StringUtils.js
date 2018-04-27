801ab38ca02cb12d4fc91f558325ba4ad193ab85
黄铖浩 <30655354+chym123@users.noreply.github.com>
2018-04-27 12:07:38 +0800

    Update StringUtils.js

--- StringUtils.js
+++ StringUtils.js
@@ -1,45 +1,51 @@
-function StrUtils() {
+;
+(function() {
+
+    function StrUtils() {
+
+    }
+
+    StrUtils.prototype = {
+        isStrOrArr: function(item) {
+            if (typeof item === 'string') {
+                return 'string';
+            }
+            if (item instanceof Array) {
+                return 'array';
+            }
+            return false;
+        },
+        swap: function(strArr, a, b) {
+            if (!this.isStrOrArr(strArr) || !strArr) {
+                return;
+            }
+            if (this.isStrOrArr(strArr) == 'string') {
+                strArr = strArr.split("");
+            }
+            let temp = strArr[a];
+            strArr[a] = strArr[b];
+            strArr[b] = temp;
+            return strArr.join("");
+        },
+        reverse: function(str) {
+            if (typeof str !== 'string' || !str) {
+                return;
+            }
+            let strArr = str.split("");
+            let p = 0,
+                q = strArr.length - 1;
+            for (; p < q; p++, q--) {
+                this.swap(strArr, p, q);
+            }
+            return strArr.join("");
+        }
+    }
+
     Object.defineProperties(StrUtils.prototype, {
         constructor: {
+            value: StrUtils,
             enumerable: false,
             configurable: false
         }
     })
-}
-
-StrUtils.prototype = {
-    constructor: StrUtils,
-    isStrOrArr: function(item) {
-        if (typeof item === 'string') {
-            return 'string';
-        }
-        if (item instanceof Array) {
-            return 'array';
-        }
-        return false;
-    },
-    swap: function(strArr, a, b) {
-        if (!this.isStrOrArr(strArr) || !strArr) {
-            return;
-        }
-        if (this.isStrOrArr(strArr) == 'string') {
-            strArr = strArr.split("");
-        }
-        let temp = strArr[a];
-        strArr[a] = strArr[b];
-        strArr[b] = temp;
-        return strArr.join("");
-    },
-    reverse: function(str) {
-        if (typeof str !== 'string' || !str) {
-            return;
-        }
-        let strArr = str.split("");
-        let p = 0,
-            q = strArr.length - 1;
-        for (; p < q; p++, q--) {
-            this.swap(strArr, p, q);
-        }
-        return strArr.join("");
-    }
-}
+})();
