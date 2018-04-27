dee81e8906c8bb44b57c0705585f4499eeb3ec83
黄铖浩 <30655354+chym123@users.noreply.github.com>
2018-04-27 11:57:52 +0800

    Update StringUtils.js

--- StringUtils.js
+++ StringUtils.js
@@ -1,13 +1,36 @@
-function StrUtils() {}
+function StrUtils() {
+    Object.defineProperties(StrUtils.prototype, {
+        constructor: {
+            enumerable: false,
+            configurable: false
+        }
+    })
+}
 
 StrUtils.prototype = {
     constructor: StrUtils,
+    isStrOrArr: function(item) {
+        if (typeof item === 'string') {
+            return 'string';
+        }
+        if (item instanceof Array) {
+            return 'array';
+        }
+        return false;
+    },
     swap: function(strArr, a, b) {
+        if (!this.isStrOrArr(strArr) || !strArr) {
+            return;
+        }
+        if (this.isStrOrArr(strArr) == 'string') {
+            strArr = strArr.split("");
+        }
         let temp = strArr[a];
         strArr[a] = strArr[b];
         strArr[b] = temp;
+        return strArr.join("");
     },
-    reverseStr: function(str) {
+    reverse: function(str) {
         if (typeof str !== 'string' || !str) {
             return;
         }
