93a2ce264779597c13c06e40bfc36fda4fb2a6c8
黄铖浩 <30655354+chym123@users.noreply.github.com>
2018-05-08 21:18:13 +0800

    Update SortUtils.js

--- src/SortUtils.js
+++ src/SortUtils.js
@@ -1,17 +1,40 @@
 ;(function(){
   
-  SortUtils = {
-    quickSort: quickSort,
-    bubbleSort: bubbleSort
-  }
+  SortUtils = {};
   
-  function swap(arr, pre, cur){
+  SortUtils.swap = function(arr, pre, cur){
     let temp = arr[pre];
     arr[pre] = arr[cur];
     arr[cur] = temp;
   }
-
-  function quickSort(arr, left, right){
+  
+  SortUtils.bubbleSort = function(arr){
+    let len = arr.length;
+    for(let i=0; i<len; i++){
+      for(let j=i; j<len; j++){
+        if(arr[i]>arr[j]){
+          this.swap(arr, i, j);
+        }
+      }
+    }
+    return arr;
+  }
+  
+  SortUtils.selectionSort = function(arr){
+    let len = arr.length;
+    for(let i=0; i<len; i++){
+      let min = i;
+      for(let j=i+1; j<len; j++){
+        if(arr[min]>arr[j]){
+          min = j;
+        }
+      }
+      swap(arr, i, min);
+    }
+    return arr;
+  }
+  
+  SortUtils.quickSort = function(arr, left, right){
     left = typeof left == 'number' ? left : 0;
     right = typeof right == 'number' ? right : arr.length;
 
@@ -24,7 +47,7 @@
 
     for(let i=store; i<right; i++){
       if(arr[i]<=arr[pos]){
-        swap(arr, store, i);
+        this.swap(arr, store, i);
         store ++;
       }
     }
@@ -34,18 +57,4 @@
     quickSort(arr, store, right);
     return arr;
   }
-  
-  function bubbleSort(arr){
-    let len = arr.length;
-    for(let i=0; i<len; i++){
-      for(let j=i; j<len; j++){
-        if(arr[i]>arr[j]){
-          swap(arr, i, j);
-        }
-      }
-    }
-    return arr;
-  }
-  
-  
 })();
